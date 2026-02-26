77fba52d8cd7ef42b5baa458dff79b0f2fe33fb5
rubybriggs <rubybriggs07@gmail.com>
2026-02-26 13:15:38 +0100

    updated structure

--- app/config.py
+++ app/config.py
@@ -8,4 +8,5 @@ class Config:
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
     AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+    AWS_REGION = os.getenv("AWS_REGION")
     VECTOR_DB_PATH = "vector_db"
